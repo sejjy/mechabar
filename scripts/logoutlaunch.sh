38376ba911ae01b3bc609ca3e4b2afbf0ef30ca3
Sejjy <mirabeljessemari@gmail.com>
2024-08-30 21:06:08 +0800

    minor fixes

--- scripts/logoutlaunch.sh
+++ scripts/logoutlaunch.sh
@@ -1,40 +1,31 @@
 #!/usr/bin/env sh
 
-
-#// Check if wlogout is already running
-
-if pgrep -x "wlogout" > /dev/null
-then
+# Check if wlogout is already running
+if pgrep -x "wlogout" > /dev/null; then
     pkill -x "wlogout"
     exit 0
 fi
 
-
-#// set file variables
-
-scrDir=`dirname "$(realpath "$0")"`
+# Set file variables
+scrDir=$(dirname "$(realpath "$0")")
 source $scrDir/globalcontrol.sh
 [ -z "${1}" ] || wlogoutStyle="${1}"
-wLayout="${confDir}/wlogout/layout_${wlogoutStyle}"
-wlTmplt="${confDir}/wlogout/style_${wlogoutStyle}.css"
+wLayout="$HOME/.config/waybar/wlogout/layout_${wlogoutStyle}"
+wlTmplt="$HOME/.config/waybar/wlogout/style_${wlogoutStyle}.css"
 
-if [ ! -f "${wLayout}" ] || [ ! -f "${wlTmplt}" ] ; then
+if [ ! -f "${wLayout}" ] || [ ! -f "${wlTmplt}" ]; then
     echo "ERROR: Config ${wlogoutStyle} not found..."
     wlogoutStyle=1
-    wLayout="${confDir}/wlogout/layout_${wlogoutStyle}"
-    wlTmplt="${confDir}/wlogout/style_${wlogoutStyle}.css"
+    wLayout="$HOME/.config/waybar/wlogout/layout_${wlogoutStyle}"
+    wlTmplt="$HOME/.config/waybar/wlogout/style_${wlogoutStyle}.css"
 fi
 
-
-#// detect monitor res
-
+# Detect monitor resolution
 x_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
 y_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
-hypr_scale=$(hyprctl -j monitors | jq '.[] | select (.focused == true) | .scale' | sed 's/\.//')
+hypr_scale=$(hyprctl -j monitors | jq '.[] | select(.focused == true) | .scale' | sed 's/\.//')
 
-
-#// scale config layout and style
-
+# Scale config layout and style
 case "${wlogoutStyle}" in
     1)  wlColms=6
         export mgn=$(( y_mon * 28 / hypr_scale ))
@@ -46,30 +37,19 @@ case "${wlogoutStyle}" in
         export y_hvr=$(( y_mon * 20 / hypr_scale )) ;;
 esac
 
-
-#// scale font size
-
+# Scale font size
 export fntSize=$(( y_mon * 2 / 100 ))
 
-
-#// detect wallpaper brightness
-
+# Detect wallpaper brightness
 [ -f "${cacheDir}/wall.dcol" ] && source "${cacheDir}/wall.dcol"
-[ "${dcol_mode}" == "dark" ] && export BtnCol="white" || export BtnCol="black"
+[ "${dcol_mode}" = "dark" ] && export BtnCol="white" || export BtnCol="black"
 
-
-#// eval hypr border radius
-
+# Evaluate hypr border radius
 export active_rad=$(( hypr_border * 5 ))
 export button_rad=$(( hypr_border * 8 ))
 
+# Evaluate config files
+wlStyle=$(envsubst < "$wlTmplt")
 
-#// eval config files
-
-wlStyle="$(envsubst < $wlTmplt)"
-
-
-#// launch wlogout
-
+# Launch wlogout
 wlogout -b "${wlColms}" -c 0 -r 0 -m 0 --layout "${wLayout}" --css <(echo "${wlStyle}") --protocol layer-shell
-
