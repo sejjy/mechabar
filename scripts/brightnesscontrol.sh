14f19deeebc557ea645ea62a91dbd9ad116d2499
Sejjy <mirabeljessemari@gmail.com>
2024-10-03 01:56:34 +0800

    fix: fix on-scroll notify

--- scripts/brightnesscontrol.sh
+++ scripts/brightnesscontrol.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-scrDir=`dirname "$(realpath "$0")"`
+scrDir=$(dirname "$(realpath "$0")")
 source $scrDir/globalcontrol.sh
 
-function print_error
-{
+# Function to print error message
+function print_error {
 cat << "EOF"
     ./brightnesscontrol.sh <action>
     ...valid actions are...
@@ -13,8 +13,9 @@ cat << "EOF"
 EOF
 }
 
+# Function to send notification with brightness info
 function send_notification {
-    brightness=`brightnessctl info | grep -oP "(?<=\()\d+(?=%)" | cat`
+    brightness=$(brightnessctl info | grep -oP "(?<=\()\d+(?=%)" | cat)
     brightinfo=$(brightnessctl info | awk -F "'" '/Device/ {print $2}')
     angle="$(((($brightness + 2) / 5) * 5))"
     ico="$HOME/.config/dunst/icons/vol/vol-${angle}.svg"
@@ -22,33 +23,37 @@ function send_notification {
     notify-send -a "t2" -r 91190 -t 800 -i "${ico}" "${brightness}${bar}" "${brightinfo}"
 }
 
+# Function to get current brightness percentage
 function get_brightness {
     brightnessctl -m | grep -o '[0-9]\+%' | head -c-2
 }
 
-case $1 in
-i)  # increase the backlight
-    if [[ $(get_brightness) -lt 10 ]] ; then
-        # increase the backlight by 1% if less than 10%
-        brightnessctl set +1%
-    else
-        # increase the backlight by 5% otherwise
-        brightnessctl set +5%
-    fi
-    send_notification ;;
-d)  # decrease the backlight
-    if [[ $(get_brightness) -le 1 ]] ; then
-        # avoid 0% brightness
-        brightnessctl set 1%
-    elif [[ $(get_brightness) -le 10 ]] ; then
-        # decrease the backlight by 1% if less than 10%
-        brightnessctl set 1%-
-    else
-        # decrease the backlight by 5% otherwise
-        brightnessctl set 5%-
-    fi
-    send_notification ;;
-*)  # print error
-    print_error ;;
-esac
-
+# Handle options
+while getopts o: opt; do
+    case "${opt}" in
+    o)
+        case $OPTARG in
+        i)  # increase the backlight
+            if [[ $(get_brightness) -lt 10 ]] ; then
+                brightnessctl set +1%
+            else
+                brightnessctl set +5%
+            fi
+            send_notification ;;
+        d)  # decrease the backlight
+            if [[ $(get_brightness) -le 1 ]] ; then
+                brightnessctl set 1%
+            elif [[ $(get_brightness) -le 10 ]] ; then
+                brightnessctl set 1%-
+            else
+                brightnessctl set 5%-
+            fi
+            send_notification ;;
+        *)
+            print_error ;;
+        esac
+        ;;
+    *)
+        print_error ;;
+    esac
+done
