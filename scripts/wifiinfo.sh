9bf3b24a59f2b6714595bea75565ee1a86d21b3e
Sejjy <mirabeljessemari@gmail.com>
2024-10-20 15:47:37 +0800

    rename essid.sh to wifiinfo.sh

--- scripts/wifiinfo.sh
+++ scripts/wifiinfo.sh
@@ -59,9 +59,19 @@
 #   6).  Example: "802.11ac" indicates you're using the 5 GHz band with
 #   a modern high-speed standard.
 
-if ! command -v nmcli &> /dev/null; then
+if ! command -v nmcli &>/dev/null; then
     echo "{\"text\": \"󰤮 Wi-Fi\", \"tooltip\": \"nmcli utility is missing\"}"
     exit 1
+fi
+
+# Check if Wi-Fi is enabled
+wifi_status=$(nmcli radio wifi)
+
+if [ "$wifi_status" = "disabled" ]; then
+    tooltip="Wi-Fi Disabled"
+    icon="󰤮" # Icon for no connection or disabled
+    echo "{\"text\": \"${icon} Wi-Fi\", \"tooltip\": \"${tooltip}\"}"
+    exit 0
 fi
 
 wifi_info=$(nmcli -t -f active,ssid,signal,security dev wifi | grep "^yes")
@@ -70,42 +80,43 @@ wifi_info=$(nmcli -t -f active,ssid,signal,security dev wifi | grep "^yes")
 if [ -z "$wifi_info" ]; then
     essid="No Connection"
     signal=0
+    tooltip="No Connection"
 else
     # Some defaults
     ip_address="127.0.0.1"
-    gateway="127.0.0.1"
-    mac_address="N/A"
+    # gateway="127.0.0.1"
+    # mac_address="N/A"
     security=$(echo "$wifi_info" | awk -F: '{print $4}')
-    bssid="N/A"
+    # bssid="N/A"
     chan="N/A"
-    rssi="N/A"
+    # rssi="N/A"
     rx_bitrate=""
     tx_bitrate=""
     phy_mode=""
     signal=$(echo "$wifi_info" | awk -F: '{print $3}')
 
-    active_device=$(nmcli -t -f DEVICE,STATE device status | \
-                        grep -w "connected" | \
-                        grep -v -E "^(dummy|lo:)" | \
-                        awk -F: '{print $1}')
+    active_device=$(nmcli -t -f DEVICE,STATE device status |
+        grep -w "connected" |
+        grep -v -E "^(dummy|lo:)" |
+        awk -F: '{print $1}')
 
     if [ -n "$active_device" ]; then
         output=$(nmcli -e no -g ip4.address,ip4.gateway,general.hwaddr device show "$active_device")
 
         ip_address=$(echo "$output" | sed -n '1p')
-        gateway=$(echo "$output" | sed -n '2p')
-        mac_address=$(echo "$output" | sed -n '3p')
+        # gateway=$(echo "$output" | sed -n '2p')
+        # mac_address=$(echo "$output" | sed -n '3p')
 
         line=$(nmcli -e no -t -f active,bssid,chan,freq device wifi | grep "^yes")
 
-        bssid=$(echo "$line" | awk -F':' '{print $2":"$3":"$4":"$5":"$6":"$7}')
+        # bssid=$(echo "$line" | awk -F':' '{print $2":"$3":"$4":"$5":"$6":"$7}')
         chan=$(echo "$line" | awk -F':' '{print $8}')
         freq=$(echo "$line" | awk -F':' '{print $9}')
         chan="$chan ($freq)"
 
-        if command -v iw &> /dev/null; then
+        if command -v iw &>/dev/null; then
             iw_output=$(iw dev "$active_device" station dump)
-            rssi=$(echo "$iw_output" | grep "signal:" | awk '{print $2 " dBm"}')
+            # rssi=$(echo "$iw_output" | grep "signal:" | awk '{print $2 " dBm"}')
 
             # Upload speed
             rx_bitrate=$(echo "$iw_output" | grep "rx bitrate:" | awk '{print $3 " " $4}')
@@ -115,52 +126,52 @@ else
 
             # Physical Layer Mode
             if echo "$iw_output" | grep -E -q "rx bitrate:.* VHT"; then
-                phy_mode="802.11ac"  # Wi-Fi 5
+                phy_mode="802.11ac" # Wi-Fi 5
             elif echo "$iw_output" | grep -E -q "rx bitrate:.* HT"; then
-                phy_mode="802.11n"  # Wi-Fi 4
+                phy_mode="802.11n" # Wi-Fi 4
             elif echo "$iw_output" | grep -E -q "rx bitrate:.* HE"; then
-                phy_mode="802.11ax"  # Wi-Fi 6
+                phy_mode="802.11ax" # Wi-Fi 6
             fi
         fi
-    fi
 
-    # Get the current Wi-Fi ESSID
-    essid=$(echo "$wifi_info" | awk -F: '{print $2}')
+        # Get the current Wi-Fi ESSID
+        essid=$(echo "$wifi_info" | awk -F: '{print $2}')
 
-    tooltip="$essid\n"
-    tooltip+="\nIP Address:  ${ip_address}"
-    tooltip+="\nRouter:      ${gateway}"
-    tooltip+="\nMAC Address: ${mac_address}"
-    tooltip+="\nSecurity:    ${security}"
-    tooltip+="\nBSSID:       ${bssid}"
-    tooltip+="\nChannel:     ${chan}"
-    tooltip+="\nRSSI:        ${rssi}"
-    tooltip+="\nSignal:      ${signal}"
+        tooltip="${essid}\n"
+        tooltip+="\nIP Address: ${ip_address}"
+        # tooltip+="\nRouter:      ${gateway}"
+        # tooltip+="\nMAC Address: ${mac_address}"
+        tooltip+="\nSecurity:   ${security}"
+        # tooltip+="\nBSSID:       ${bssid}"
+        tooltip+="\nChannel:    ${chan}"
+        # tooltip+="\nRSSI:        ${rssi}"
+        tooltip+="\nStrength:   ${signal} / 100"
 
-    if [ -n "$rx_bitrate" ]; then
-        tooltip+="\nRx Rate:     ${rx_bitrate}"
-    fi
+        if [ -n "$rx_bitrate" ]; then
+            tooltip+="\nRx Rate:     ${rx_bitrate}"
+        fi
 
-    if [ -n "$tx_bitrate" ]; then
-        tooltip+="\nTx Rate:     ${tx_bitrate}"
-    fi
+        if [ -n "$tx_bitrate" ]; then
+            tooltip+="\nTx Rate:     ${tx_bitrate}"
+        fi
 
-    if [ -n "$phy_mode" ]; then
-        tooltip+="\nPHY Mode:    ${phy_mode}"
+        if [ -n "$phy_mode" ]; then
+            tooltip+="\nPHY Mode:    ${phy_mode}"
+        fi
     fi
 fi
 
 # Determine Wi-Fi icon based on signal strength
 if [ "$signal" -ge 80 ]; then
-    icon="󰤨"  # Strong signal
+    icon="󰤨" # Strong signal
 elif [ "$signal" -ge 60 ]; then
-    icon="󰤥"  # Good signal
+    icon="󰤥" # Good signal
 elif [ "$signal" -ge 40 ]; then
-    icon="󰤢"  # Weak signal
+    icon="󰤢" # Weak signal
 elif [ "$signal" -ge 20 ]; then
-    icon="󰤟"  # Very weak signal
+    icon="󰤟" # Very weak signal
 else
-    icon="󰤮"  # No signal
+    icon="󰤮" # No signal
 fi
 
 # Change "Wi-Fi" to "${essid}" to display network name
