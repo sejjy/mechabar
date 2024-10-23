053f163a9b7791254f047e3fb3e246d2769e253a
Sejjy <mirabeljessemari@gmail.com>
2024-10-23 17:29:34 +0800

    modify if statement for consistency

--- scripts/cpuusage.sh
+++ scripts/cpuusage.sh
@@ -7,19 +7,19 @@ model=$(awk -F ': ' '/model name/{print $2}' /proc/cpuinfo | head -n 1 | sed 's/
 load=$(vmstat 1 2 | tail -1 | awk '{print 100 - $15}')
 
 # Determine CPU state based on usage
-if [ "$load" -le 25 ]; then
-    state="Low"
-elif [ "$load" -le 60 ]; then
-    state="Moderate"
-elif [ "$load" -le 80 ]; then
-    state="High"
-else
+if [ "$load" -ge 80 ]; then
     state="Critical"
+elif [ "$load" -ge 60 ]; then
+    state="High"
+elif [ "$load" -ge 25 ]; then
+    state="Moderate"
+else
+    state="Low"
 fi
 
 # Set color based on CPU load
-if [ "$load" -gt 80 ]; then
-    # If CPU usage is above 80%, set color to #f38ba8
+if [ "$load" -ge 80 ]; then
+    # If CPU usage is >= 80%, set color to #f38ba8
     text_output="<span color='#f38ba8'>󰀩 ${load}%</span>"
 else
     # Default color
