454c6bb3480325761b7044cd7bc8161d2c6a375b
Sejjy <sejjymvm@gmail.com>
2024-10-30 11:58:34 +0800

    refactor: improve functionality

--- scripts/logoutlaunch.sh
+++ scripts/logoutlaunch.sh
@@ -1,60 +1,51 @@
 #!/usr/bin/env bash
 
-# Check if wlogout is already running
-if pgrep -x "wlogout" > /dev/null; then
-    pkill -x "wlogout"
-    exit 0
+# Check if wlogout is already running, and terminate if so
+if pgrep -x "wlogout" >/dev/null; then
+  pkill -x "wlogout"
+  exit 0
 fi
 
 # Define configuration directories
-confDir="${XDG_CONFIG_HOME:-$HOME/.config}"
+config="$HOME/.config/wlogout"
 
 # Style selection
-[ -z "${1}" ] || wlogoutStyle="${1}"
-wLayout="${confDir}/wlogout/layout_${wlogoutStyle}"
-wlTmplt="${confDir}/wlogout/style_${wlogoutStyle}.css"
+[ -z "${1}" ] || style_index="${1}"
+layout="${config}/layout-${style_index}"
+style="${config}/style-${style_index}.css"
 
-if [ ! -f "${wLayout}" ] || [ ! -f "${wlTmplt}" ]; then
-    echo "ERROR: Config ${wlogoutStyle} not found..."
-    wlogoutStyle=1
-    wLayout="${confDir}/wlogout/layout_${wlogoutStyle}"
-    wlTmplt="${confDir}/wlogout/style_${wlogoutStyle}.css"
+# Fallback if style or layout file is not found
+if [ ! -f "${layout}" ] || [ ! -f "${style}" ]; then
+  echo "ERROR: Config ${style_index} not found..."
+  style_index=1
+  layout="${config}/layout-${style_index}"
+  style="${config}/style-${style_index}.css"
 fi
 
-# Detect monitor resolution
-x_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
-y_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
-hypr_scale=$(hyprctl -j monitors | jq '.[] | select(.focused == true) | .scale' | sed 's/\.//')
+# Detect monitor resolution and scaling
+screen_width=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
+screen_height=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
+scale_factor=$(hyprctl -j monitors | jq '.[] | select(.focused == true) | .scale' | sed 's/\.//')
 
-# Scale config layout and style
-case "${wlogoutStyle}" in
-    1)
-        wlColms=6
-        export mgn=$(( y_mon * 38 / hypr_scale ))
-        export hvr=$(( y_mon * 33 / hypr_scale ))
-        ;;
-    2)
-        wlColms=2
-        export x_mgn=$(( x_mon * 38 / hypr_scale ))
-        export y_mgn=$(( y_mon * 28 / hypr_scale ))
-        export x_hvr=$(( x_mon * 35 / hypr_scale ))
-        export y_hvr=$(( y_mon * 23 / hypr_scale ))
-        ;;
+# Scale configuration based on selected layout
+case "${style_index}" in
+1)
+  button_columns=6
+  export margin=$((screen_height * 38 / scale_factor))
+  export hover_margin=$((screen_height * 33 / scale_factor))
+  ;;
+2)
+  button_columns=2
+  export x_margin=$((screen_width * 38 / scale_factor))
+  export y_margin=$((screen_height * 28 / scale_factor))
+
+  export x_hover=$((screen_width * 35 / scale_factor))
+  export y_hover=$((screen_height * 23 / scale_factor))
+  ;;
 esac
 
-# Scale font size
-export fntSize=$(( y_mon * 2 / 100 ))
+# Substitute variables in the style template
+style_content=$(envsubst <"$style")
 
-export BtnCol="white"  # Default button color
-
-# Evaluate hypr border radius
-hypr_border="$(hyprctl -j getoption decoration:rounding | jq '.int')"
-export hypr_border
-export active_rad=$(( hypr_border * 5 ))
-export button_rad=$(( hypr_border * 8 ))
-
-# Evaluate config files
-wlStyle=$(envsubst < "$wlTmplt")
-
-# Launch wlogout
-wlogout -b "${wlColms}" -c 0 -r 0 -m 0 --layout "${wLayout}" --css <(echo "${wlStyle}") --protocol layer-shell
+# Launch wlogout with the specified layout and style
+wlogout -b "${button_columns}" -c 0 -r 0 -m 0 --layout "${layout}" --css <(echo "${style_content}") --protocol layer-shell
