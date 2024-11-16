bdb9087315bef7e4058d331fc6d8744be0c5cdeb
Sejjy <sejjymvm@gmail.com>
2024-11-16 17:56:41 +0800

    style: update window title for system update script

--- scripts/systemupdate.sh
+++ scripts/systemupdate.sh
@@ -40,7 +40,7 @@ if [ "$1" == "up" ]; then
     printf '\n'
     read -n 1 -p 'Press any key to continue...'
     "
-  kitty --title "System Update" sh -c "${command}"
+  kitty --title "󰞒  System Update" sh -c "${command}"
 fi
 
 # Check for AUR updates
