b5251e0ff28894a535cd0e51ad4c03d494210d78
Sejjy <sejjymvm@gmail.com>
2024-11-25 20:06:48 +0800

    refactor: streamline logout menu script

--- scripts/logout-menu.sh
+++ scripts/logout-menu.sh
@@ -1,51 +1,32 @@
 #!/usr/bin/env bash
 
-# Check if wlogout is already running, and terminate if so
+# Check if wlogout is already running
 if pgrep -x "wlogout" >/dev/null; then
   pkill -x "wlogout"
   exit 0
 fi
 
-# Define configuration directories
+# File paths
 config="$HOME/.config/wlogout"
+layout="${config}/layout"
+style="${config}/style.css"
 
-# Style selection
-[ -z "${1}" ] || style_index="${1}"
-layout="${config}/layout-${style_index}"
-style="${config}/style-${style_index}.css"
-
-# Fallback if style or layout file is not found
+# Check if required configuration files exist
 if [ ! -f "${layout}" ] || [ ! -f "${style}" ]; then
-  echo "ERROR: Config ${style_index} not found..."
-  style_index=1
-  layout="${config}/layout-${style_index}"
-  style="${config}/style-${style_index}.css"
+  echo "ERROR: Required configuration files not found."
+  exit 1
 fi
 
-# Detect monitor resolution and scaling
-screen_width=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
-screen_height=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
-scale_factor=$(hyprctl -j monitors | jq '.[] | select(.focused == true) | .scale' | sed 's/\.//')
+# Get monitor information
+width=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
+height=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
+scale=$(hyprctl -j monitors | jq '.[] | select(.focused == true) | .scale' | sed 's/\.//')
 
-# Scale configuration based on selected layout
-case "${style_index}" in
-1)
-  button_columns=6
-  export margin=$((screen_height * 38 / scale_factor))
-  export hover_margin=$((screen_height * 33 / scale_factor))
-  ;;
-2)
-  button_columns=2
-  export x_margin=$((screen_width * 38 / scale_factor))
-  export y_margin=$((screen_height * 28 / scale_factor))
+# Calculate margins
+export x_margin=$((width * 32 / scale))
+export y_margin=$((height * 28 / scale))
 
-  export x_hover=$((screen_width * 35 / scale_factor))
-  export y_hover=$((screen_height * 23 / scale_factor))
-  ;;
-esac
+stylesheet=$(envsubst <"$style")
 
-# Substitute variables in the style template
-style_content=$(envsubst <"$style")
-
-# Launch wlogout with the specified layout and style
-wlogout -b "${button_columns}" -c 0 -r 0 -m 0 --layout "${layout}" --css <(echo "${style_content}") --protocol layer-shell
+# Launch wlogout
+wlogout -b 3 -c 0 -r 0 -m 0 --layout "${layout}" --css <(echo "${stylesheet}") --protocol layer-shell
