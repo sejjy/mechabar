e4ef86c0bfd840506cd365b61958e2c9e68c751b
sejjy <sejjymvm@gmail.com>
2025-02-07 20:38:32 +0800

    fix: replace hyprlock and hyprctl commands with loginctl for session management

--- scripts/power-menu.sh
+++ scripts/power-menu.sh
@@ -10,7 +10,7 @@ selected_option=$(echo -e "$actions" | rofi -dmenu -i -config "${config}" || pki
 # Perform actions based on the selected option
 case "$selected_option" in
 *Lock)
-  hyprlock
+  loginctl lock-session
   ;;
 *Shutdown)
   systemctl poweroff
@@ -25,6 +25,6 @@ case "$selected_option" in
   systemctl hibernate
   ;;
 *Logout)
-  hyprctl dispatch exit 0
+  loginctl kill-user "$(whoami)"
   ;;
 esac
