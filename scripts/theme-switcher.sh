6438af2016902f391f86f5558a54af9887fb220c
sejjy <sejjymvm@gmail.com>
2025-02-20 02:38:41 +0800

    feat: add rofi themes

--- scripts/theme-switcher.sh
+++ scripts/theme-switcher.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 
-THEMES_DIR="$HOME/.config/waybar/themes"
-THEME_FILE="$HOME/.config/waybar/theme.css"
+WAYBAR_THEMES_DIR="$HOME/.config/waybar/themes"
+WAYBAR_THEME_FILE="$HOME/.config/waybar/theme.css"
+ROFI_THEMES_DIR="$HOME/.config/rofi/themes"
+ROFI_THEME_FILE="$HOME/.config/rofi/theme.rasi"
 CURRENT_THEME_FILE="$HOME/.config/waybar/current-theme"
 
-if [[ ! -d "$THEMES_DIR" ]]; then
-  echo "Error: Themes directory does not exist."
+if [[ ! -d "$WAYBAR_THEMES_DIR" ]]; then
+  echo "Error: $WAYBAR_THEMES_DIR not found"
+  exit 1
+elif [[ ! -d "$ROFI_THEMES_DIR" ]]; then
+  echo "Error: $ROFI_THEMES_DIR not found"
   exit 1
 fi
 
 # Get all themes
-THEMES=("$THEMES_DIR"/*.css)
-THEME_COUNT=${#THEMES[@]}
+WAYBAR_THEMES=("$WAYBAR_THEMES_DIR"/*.css)
+WAYBAR_THEME_COUNT=${#WAYBAR_THEMES[@]}
+ROFI_THEMES=("$ROFI_THEMES_DIR"/*.rasi)
+ROFI_THEME_COUNT=${#ROFI_THEMES[@]}
 
-if [[ $THEME_COUNT -eq 0 ]]; then
-  echo "Error: No themes found in $THEMES_DIR"
+if [[ $WAYBAR_THEME_COUNT -eq 0 ]]; then
+  echo "Error: No themes found in $WAYBAR_THEMES_DIR"
+  exit 1
+elif [[ $ROFI_THEME_COUNT -eq 0 ]]; then
+  echo "Error: No themes found in $ROFI_THEMES_DIR"
   exit 1
 fi
 
@@ -27,20 +37,29 @@ fi
 
 NEXT_THEME_INDEX=0
 
-for i in "${!THEMES[@]}"; do
-  if [[ "${THEMES[$i]}" == "$CURRENT_THEME" ]]; then
-    NEXT_THEME_INDEX=$(((i + 1) % THEME_COUNT))
+for i in "${!WAYBAR_THEMES[@]}"; do
+  if [[ "${WAYBAR_THEMES[$i]}" == "$CURRENT_THEME" ]]; then
+    NEXT_THEME_INDEX=$(((i + 1) % WAYBAR_THEME_COUNT))
     break
   fi
 done
 
-NEW_THEME="${THEMES[$NEXT_THEME_INDEX]}"
+for i in "${!ROFI_THEMES[@]}"; do
+  if [[ "${ROFI_THEMES[$i]}" == "$CURRENT_THEME" ]]; then
+    NEXT_THEME_INDEX=$(((i + 1) % ROFI_THEME_COUNT))
+    break
+  fi
+done
+
+NEW_WAYBAR_THEME="${WAYBAR_THEMES[$NEXT_THEME_INDEX]}"
+NEW_ROFI_THEME="${ROFI_THEMES[$NEXT_THEME_INDEX]}"
 
 # Save the new theme
-echo "$NEW_THEME" >"$CURRENT_THEME_FILE"
+echo "$NEW_WAYBAR_THEME" >"$CURRENT_THEME_FILE"
 
 # Apply new theme
-cp "$NEW_THEME" "$THEME_FILE"
+cp "$NEW_WAYBAR_THEME" "$WAYBAR_THEME_FILE"
+cp "$NEW_ROFI_THEME" "$ROFI_THEME_FILE"
 
 # Restart Waybar to apply changes
 killall waybar || true
