88ad3932f7896eed31e61d9af036b40e622e0e77
sejjy <sejjymvm@gmail.com>
2025-02-20 17:12:50 +0800

    fix: set default theme name

--- scripts/current-theme.sh
+++ scripts/current-theme.sh
@@ -4,7 +4,7 @@ CURRENT_THEME_FILE="$HOME/.config/waybar/themes/current-theme"
 
 # Get the current theme
 current_theme=$(cat "$CURRENT_THEME_FILE" 2>/dev/null || echo "")
-current_theme_name="Unknown"
+current_theme_name="Default"
 
 # Get the theme name
 if [[ -n "$current_theme" ]]; then
@@ -14,7 +14,7 @@ if [[ -n "$current_theme" ]]; then
   formatted_theme_name="${current_theme_name//-/ }"
   formatted_theme_name=$(echo "$formatted_theme_name" | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)}1')
 else
-  formatted_theme_name="Unknown"
+  formatted_theme_name="Default"
 fi
 
 tooltip="Theme: $formatted_theme_name"
