e7f861dae33f1b8a0166f91749bbe89034a67c25
Jesse Mirabel <sejjymvm@gmail.com>
2025-03-17 13:11:57 +0800

    fix: integer expression expected (#13)

--- scripts/cpu-temp.sh
+++ scripts/cpu-temp.sh
@@ -2,70 +2,66 @@
 
 model=$(awk -F ': ' '/model name/{print $2}' /proc/cpuinfo | head -n 1 | sed 's/@.*//; s/ *\((R)\|(TM)\)//g; s/^[ \t]*//; s/[ \t]*$//')
 
-# Get CPU clock speeds
+# get CPU clock speeds
 get_cpu_frequency() {
   freqlist=$(awk '/cpu MHz/ {print $4}' /proc/cpuinfo)
-  maxfreq=$(sed 's/...$//' /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq 2>/dev/null || echo "N/A")
-  if [ -n "$freqlist" ]; then
-    average_freq=$(echo "$freqlist" | tr ' ' '\n' | awk "{sum+=\$1} END {printf \"%.0f/%s MHz\", sum/NR, \"$maxfreq\"}")
-  else
-    average_freq="N/A"
+  maxfreq=$(sed 's/...$//' /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq)
+  if [ -z "$freqlist" ] || [ -z "$maxfreq" ]; then
+    echo "N/A"
+    return
   fi
+  average_freq=$(echo "$freqlist" | tr ' ' '\n' | awk "{sum+=\$1} END {printf \"%.0f/%s MHz\", sum/NR, $maxfreq}")
   echo "$average_freq"
 }
 
-# Get CPU temperature
+# get CPU temp
 get_cpu_temperature() {
-  temp=$(sensors | awk '/Package id 0/ {print $4}' | awk -F '[+.]' '{print $2}' 2>/dev/null)
+  temp=$(sensors | awk '/Package id 0/ {print $4}' | awk -F '[+.]' '{print $2}')
   if [[ -z "$temp" ]]; then
-    temp=$(sensors | awk '/Tctl/ {print $2}' | tr -d '+°C' 2>/dev/null)
+    temp=$(sensors | awk '/Tctl/ {print $2}' | tr -d '+°C')
   fi
-  if [[ -z "$temp" || ! "$temp" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+  if [[ -z "$temp" ]]; then
     temp="N/A"
     temp_f="N/A"
   else
-    # Convertir a entero para comparaciones, pero mantener decimal para mostrar
-    temp_int=${temp%.*}  # Tomar la parte entera
+    temp=${temp%.*}
     temp_f=$(awk "BEGIN {printf \"%.1f\", ($temp * 9 / 5) + 32}")
   fi
-  echo "$temp $temp_f $temp_int"
+  # Celsius and Fahrenheit
+  echo "${temp:-N/A} ${temp_f:-N/A}"
 }
 
-# Get the corresponding icon based on temperature
 get_temperature_icon() {
   temp_value=$1
   if [ "$temp_value" = "N/A" ]; then
-    icon="󱃃" # Default icon when temp is not available
+    icon="󱔱" # none
   elif [ "$temp_value" -ge 80 ]; then
-    icon="󰸁" # High temperature
+    icon="󰸁" # high
   elif [ "$temp_value" -ge 70 ]; then
-    icon="󱃂" # Medium temperature
+    icon="󱃂" # medium
   elif [ "$temp_value" -ge 60 ]; then
-    icon="󰔏" # Normal temperature
+    icon="󰔏" # normal
   else
-    icon="󱃃" # Low temperature
+    icon="󱃃" # low
   fi
   echo "$icon"
 }
 
-# Main script execution
 cpu_frequency=$(get_cpu_frequency)
-read -r temp temp_f temp_int < <(get_cpu_temperature)
+read -r temp_info < <(get_cpu_temperature)
+temp=$(echo "$temp_info" | awk '{print $1}')
+temp_f=$(echo "$temp_info" | awk '{print $2}')
+thermo_icon=$(get_temperature_icon "$temp")
 
-# Determine the temperature icon using the integer value
-thermo_icon=$(get_temperature_icon "${temp_int:-0}")
-
-# Set color based on temperature
-if [ "$temp_int" != "N/A" ] && [ "$temp_int" -ge 80 ]; then
-  # If temperature is >= 80°C, set color to #f38ba8
+# high temp warning
+if [ "$temp" != "N/A" ] && [ "$temp" -ge 80 ]; then
   text_output="<span color='#f38ba8'>${thermo_icon} ${temp}°C</span>"
 else
-  # Default color
   text_output="${thermo_icon} ${temp}°C"
 fi
 
-tooltip="${model}\n"
+tooltip=":: ${model}\n"
 tooltip+="Clock Speed: ${cpu_frequency}\nTemperature: ${temp_f}°F"
 
-# Module and tooltip
+# module and tooltip
 echo "{\"text\": \"$text_output\", \"tooltip\": \"$tooltip\"}"
