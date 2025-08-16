5e4fa88b9a63802dfddff3c5234752921ef18a2a
sejjy <sejjymvm@gmail.com>
2025-08-16 19:33:45 +0800

    [feat] display update counts for official and AUR packages

--- scripts/system-update.sh
+++ scripts/system-update.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
+#
+# Check for system updates using pacman and AUR helpers
+#
+# Author: Jesse Mirabel <github.com/sejjy>
+# Created: August 16, 2025
+# License: MIT
 
-echo "{\"text\": \" \", \"tooltip\": \"\"}"
+repo_updates=$(pacman -Quq | wc -l)
+
+helper=$(
+	basename "$(
+		command -v yay trizen pikaur paru pakku pacaur aurman aura 2>/dev/null |
+			head -n 1
+	)"
+)
+
+if [[ -z $helper ]]; then
+	helper="none"
+	aur_updates=0
+else
+	aur_updates=$($helper -Quaq | wc -l)
+fi
+
+tooltip="Pacman: $repo_updates\nAUR ($helper): $aur_updates"
+
+echo "{\"text\": \" \", \"tooltip\": \"$tooltip\"}"
