e55d29f4247dbcdfeec9b226d9616faa75548651
sejjy <sejjymvm@gmail.com>
2025-10-28 20:25:53 +0800

    refactor: dynamically parse theme colors for fzf
    - Extract hex codes from theme.css instead of hardcoding them

--- scripts/fzf-colors.sh
+++ scripts/fzf-colors.sh
@@ -1,41 +1,66 @@
 #!/usr/bin/env bash
 
-main() {
-	local file=$XDG_CONFIG_HOME/waybar/theme.css
+FILE="$XDG_CONFIG_HOME/waybar/theme.css"
+
+def-colors() {
 	local theme
-	theme=$(head -n 1 "$file" | awk '{print $2}')
-	local rosewater mauve red lavender text overlay0 surface1 surface0 base
+	theme=$(sed 1q "$FILE")
 
-	case $theme in
-		*"frappe")
-			rosewater='#f2d5cf' mauve='#ca9ee6'    red='#e78284'
-			lavender='#babbf1'  text='#c6d0f5'     overlay0='#737994'
-			surface1='#51576d'  surface0='#414559' base='#303446'
-			;;
-		*"latte")
-			rosewater='#dc8a78' mauve='#8839ef'    red='#d20f39'
-			lavender='#7287fd'  text='#4c4f69'     overlay0='#9ca0b0'
-			surface1='#bcc0cc'  surface0='#ccd0da' base='#eff1f5'
-			;;
-		*"macchiato")
-			rosewater='#f4dbd6' mauve='#c6a0f6'    red='#ed8796'
-			lavender='#b7bdf8'  text='#cad3f5'     overlay0='#6e738d'
-			surface1='#494d64'  surface0='#363a4f' base='#24273a'
-			;;
-		*"mocha")
-			rosewater='#f5e0dc' mauve='#cba6f7'    red='#f38ba8'
-			lavender='#b4befe'  text='#cdd6f4'     overlay0='#6c7086'
-			surface1='#45475a'  surface0='#313244' base='#1e1e2e'
-			;;
-	esac
+	declare -ga names
 
-	export COLORS=(
-		--color="bg+:$surface0,bg:$base,spinner:$rosewater,hl:$red"
-		--color="fg:$text,header:$red,info:$mauve,pointer:$rosewater"
-		--color="marker:$lavender,fg+:$text,prompt:$mauve,hl+:$red"
-		--color="selected-bg:$surface1"
-		--color="border:$overlay0,label:$text"
+	# add themes here
+	if [[ $theme == *"catppuccin"* ]]; then
+		names=(
+			'surface0' 'base' 'rosewater' 'red' 'text' 'red' 'mauve' 'rosewater'
+			'lavender' 'text' 'mauve' 'red' 'surface1' 'overlay0' 'text'
+		)
+	fi
+}
+
+get-hex() {
+	local defs
+	defs=$(sed -n '3,28p' "$FILE")
+
+	local n hex
+	declare -gA colors
+
+	for n in "${names[@]}"; do
+		read -r _ _ hex < <(grep " $n " <<< "$defs")
+		hex=${hex%;}
+
+		colors[$n]=$hex
+	done
+}
+
+map-colors() {
+	local -a maps=(
+		'bgp' 'bg' 'spinner' 'hl' 'fg' 'header' 'info' 'pointer' 'marker' 'fgp'
+		'prompt' 'hlp' 'selected_bg' 'border' 'label'
 	)
+
+	local n
+	local i=0
+
+	for n in "${names[@]}"; do
+		declare -g "${maps[i]}"="${colors[$n]}"
+		((i++))
+	done
+}
+
+main() {
+	def-colors
+	get-hex
+	map-colors
+
+	declare -ga COLORS=(
+		"--color=bg+:${bgp:?},bg:${bg:?},spinner:${spinner:?},hl:${hl:?}"
+		"--color=fg:${fg:?},header:${header:?},info:${info:?}"
+		"--color=pointer:${pointer:?},marker:${marker:?},fg+:${fgp:?}"
+		"--color=prompt:${prompt:?},hl+:${hlp:?},selected-bg:${selected_bg:?}"
+		"--color=border:${border:?},label:${label:?}"
+	)
+
+	export COLORS
 }
 
 main
